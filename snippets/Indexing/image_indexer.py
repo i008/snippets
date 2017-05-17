433db2d98052329986161fbbe2952477c5af2880
Jakub Cieslik <kubacieslik@gmail.com>
2017-05-17 12:29:05 +0200

    Update image_indexer.py

--- snippets/Indexing/image_indexer.py
+++ snippets/Indexing/image_indexer.py
@@ -21,21 +21,21 @@ class ImageIndexer(object):
     #         self.db_index_buffer = []
 
     def __enter__(self):
-        print "indexing {} images".format(self.num_of_images)
+        print("indexing {} images".format(self.num_of_images))
         self.t0 = time.time()
         return self
 
     def __exit__(self, exc_type, exc_val, exc_tb):
         if self.image_id_buffer:
-            print "writing last buffers"
+            print("writing last buffers")
             print(len(self.image_id_buffer))
 
             self._write_buffer(self.image_id_db, self.image_id_buffer)
             self._write_buffer(self.image_vector_db, self.image_vector_buffer)
 
-        print "closing h5 db"
+        print("closing h5 db")
         self.db.close()
-        print "indexing took {0}".format(time.time() - self.t0)
+        print("indexing took {0}".format(time.time() - self.t0))
 
     @property
     def image_vector_size(self):
@@ -45,19 +45,20 @@ class ImageIndexer(object):
             return None
 
     def create_datasets(self):
+        
+        IMG_ROWS, IMG_COLS, CHANN = self.fixed_image_shape
 
         self.image_id_db = self.db.create_dataset(
             "image_ids",
             (self.num_of_images,),
             maxshape=None,
-            dtype=h5py.special_dtype(vlen=unicode)
+            dtype=h5py.special_dtype(vlen=str)
 
         )
 
         self.image_vector_db = self.db.create_dataset(
-            "image_vectors",
-            (self.num_of_images, self.image_vector_size),
-            maxshape=(None, self.image_vector_size),
+            "images",
+            shape=(self.num_of_images,IMG_ROWS, IMG_COLS, CHANN),
             dtype="float"
         )
 
@@ -79,7 +80,7 @@ class ImageIndexer(object):
             self._clean_buffers()
 
     def _write_buffer(self, dataset, buf):
-        print "Writing buffer {}".format(dataset)
+        print("Writing buffer {}".format(dataset))
         start = self.idxs['index']
         end = len(buf)
         dataset[start:start + end] = buf
